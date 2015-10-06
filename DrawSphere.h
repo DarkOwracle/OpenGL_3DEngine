028e5f237ef9d6581f7a5e6746ab4ac3f9ccddd4
syldreams <koch.syl@gmail.com>
2015-10-06 14:31:56 +0200

    Create DrawSphere.h

---
+++ DrawSphere.h
@@ -0,0 +1,11 @@
+#if !defined(DRAWSPHERE_H)
+#define DRAWSPHERE_H
+
+typedef struct{
+    float m_Radius;
+    int m_Sector;
+}TShapeSphere;
+
+void DrawSphere(void* pT);
+
+#endif // !defined(DRAWSPHERE_H)
