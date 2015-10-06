f21312eb239f5e0c4fcf0e7e43d246d61c2aff85
Rocail <rocaile3@gmail.com>
2015-10-06 14:27:39 +0200

    Update DrawCone.c

--- DrawCone.c
+++ DrawCone.c
@@ -9,55 +9,33 @@
 #include <GL\glu.h>
 
 #include "Definition.h"
-#include "MathLib.h"
 
-
-//La fonction dessine un cylindre
-//Le cylindre est un assemblage de : triangle + rectangle + triangle
-void DrawCone(void *pT)
+void DrawCone(void* pT)
 {
-//Recupere les donnees du cylindre
+     glEnable(GL_NORMALIZE);
+   //Recupere les donnees de la sphere
     TShapeCone *p = (TShapeCone*)pT;
-    float radius= p->m_Radius;
-    int sector= p->m_Sector;
-    int height= p->m_Size;
-//Dessin
-    double dp = 2.0f*PI/(double)sector; //On a l'angle: (le prériphérique d'un cercle 2PI disisé par le nombre de morceau choisi)
+    float radius=p->m_Radius;
+    int sector=p->m_Sector;
+    float height=p->m_Height;
 
-    //positions du points
-    double p1x,p1y,p1z;
-    double p2x,p2y,p2z;
-    double p3x,p3y,p3z;
-
-    int j;
-
-    //Pour les faces rectangulaires
-    for(j=0; j<sector; j++)
+    double dp = 2.0f*PI/(double)sector;
+    float alpha = atan(radius/height);
+    int i;
+    for(i=0; i<sector; i++)
     {
-        //calcule des positions x y z
-        p1x = cos(dp *j); p1y = atan(1/height); p1z = sin(dp*j);
-        p2x = 0; p2y = 1; p2z = 0;
-        p3x = cos(dp+ dp*j); p3y = 0; p3z = sin(dp+ dp*j);
+        glBegin(GL_TRIANGLES);
+        glNormal3f(0,-1,0); 
+        glVertex3f(0,0,0);
+        glVertex3f((radius*cos(i*dp)),0,(radius*sin(i*dp)));
+        glVertex3f((radius*cos((i+1)*dp)),0,(radius*sin((i+1)*dp)));
         
-
-        //Création d'un rectangle
-        glBegin(GL_TRIANGLES);
-            glNormal3f(p1x,p1y,p1z);
-            glVertex3f(p1x*radius,0.0,p1z*radius);
-            glNormal3f(p2x,p2y,p2z);
-            glVertex3f(0.0,height,0.0);
-            glNormal3f(p3x,p3y,p3z);
-            glVertex3f(p3x*radius,0.0,p3z*radius);
-        glEnd();
-
-        //Création du triangle du dessous
-        glBegin(GL_TRIANGLES);
-            glNormal3f(0.0,-1.0f,0.0);
-            glVertex3f(0,0,0);
-            //glNormal3f(0.0,-1.0f,0.0);
-            glVertex3f(p1x*radius,0.0,p1z*radius);
-            //glNormal3f(0.0,-1.0f,0.0);
-            glVertex3f(p3x*radius,0.0,p3z*radius);
+        glNormal3f(cos(i*dp)*cos(alpha),sin(alpha),cos(alpha)*sin(i*dp));
+        glVertex3f((radius*cos(i*dp)),0,(radius*sin(i*dp)));
+        glVertex3f((radius*cos((i+1)*dp)),0,(radius*sin((i+1)*dp)));
+        glVertex3f(0,height,0);
         glEnd();
     }
+
+
 }
