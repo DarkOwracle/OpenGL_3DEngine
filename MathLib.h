b4a5eb994294e9dacf75b415d8ca95fa612b9f1f
syldreams <koch.syl@gmail.com>
2015-10-06 14:41:25 +0200

    Create MathLib.h

---
+++ MathLib.h
@@ -0,0 +1,8 @@
+#if !defined(MATHLIB_H)
+#define MATHLIB_H
+
+void PointOnUnitSphere(const double azim, const double zeni, double *px, double *py, double *pz);
+
+int GetRand(int minV, int maxV);
+
+#endif // !defined(MATHLIB_H)
