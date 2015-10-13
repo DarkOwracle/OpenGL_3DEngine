9f6218790e8bce65ac1bf2ad02c7757c1c20c8fd
Rocail <rocaile3@gmail.com>
2015-10-13 14:46:49 +0200

    Create DrawBite.h

---
+++ DrawBite.h
@@ -0,0 +1,12 @@
+#if !defined(DRAWBITE_H)
+#define DRAWBITE_H
+
+typedef struct{
+    float m_Radius;
+    int m_Sector;
+    float m_Height;
+}TShapeBite;
+
+void DrawBite(void* pT);
+
+#endif // !defined(DRAWCONE_H)
