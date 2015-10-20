aebf5dc2dee7be655e6a67a3e83afe849ae23223
Rocail <rocaile3@gmail.com>
2015-10-20 17:12:30 +0200

    Début des textures

--- Material.h
+++ Material.h
@@ -1,14 +1,16 @@
 #if !defined(MATERIAL_H)
 #define MATERIAL_H
 
+#include "Texture.h"
+
 //Structure d'un materiau
 typedef struct{
     float m_Kd[4];  //Diffusion
     float m_Ke[4];  //Emissivite
-
+    TTexture *m_Texture;
 }TMaterial;
 
-TMaterial* CreateMaterial(float Kd[], float Ke[]);
+TMaterial* CreateMaterial(float Kd[], float Ke[], char* texturename);
 void DrawMaterial(TMaterial *pmat);
 
 #endif // !defined(MATERIAL_H)
